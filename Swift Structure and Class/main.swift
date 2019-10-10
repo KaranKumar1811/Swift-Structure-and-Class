503a07abdedf40d5262d0dd2090c5c7502891677
Karan <kumarkay.ca@gmail.com>
2019-10-10 19:30:21 -0400

    Initial Commit

---
+++ Swift Structure and Class/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Structure and Class
+//
+//  Created by MacStudent on 2019-10-10.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
